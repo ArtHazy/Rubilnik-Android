3428c3e1913fb628949ff36df00c3e9d8bc900ca
Константин <kos.ryb@mail.ru>
2023-12-08 13:18:53 +0300

    v1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,14 @@ android {
 
     defaultConfig {
         applicationId = "com.example.rubilnik"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        viewBinding.enable = true
     }
 
     buildTypes {
@@ -36,6 +38,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.compose.ui:ui-unit-android:1.5.4")
+    implementation("androidx.compose.foundation:foundation-layout-android:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.navigation:navigation-fragment:2.7.5")
+    implementation("androidx.navigation:navigation-ui:2.7.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/rubilnik/MainActivity.java
+++ app/src/main/java/com/example/rubilnik/MainActivity.java
@@ -1,101 +1,168 @@
 package com.example.rubilnik;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity implements SetUserNameDialog.activityContains_getStringFromDialog {
-
-    TextView usernameTextView;
-
-    Button usernameButton;
-    Button connectButton;
-    ImageButton editUsernameImageButton;
-
-    LinearLayout settingsLinLay;
-    LinearLayout settingsGroup;
-    Context context;
+import com.example.rubilnik.databinding.ActivityMainBinding;
+import com.example.rubilnik.screens.first.FirstFragment;
+import com.example.rubilnik.screens.second.SecondFragment;
+import com.example.rubilnik.screens.third.ThirdFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 
+public class MainActivity extends AppCompatActivity {
+
+//    TextView usernameTextView;
+//
+//    Button usernameButton;
+//    Button connectButton;
+//    ImageButton editUsernameImageButton;
+//
+//    LinearLayout settingsLinLay;
+//    LinearLayout settingsGroup;
+//    Context context;
+//
+//    EditText editText;
+//
+//    ActivityMainBinding binding;
+//    NavController navController;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile);
+        setContentView(R.layout.activity_main);
 
-        context = this;
-
-        settingsLinLay = findViewById(R.id.settings_lin_lay);
-
-        ArrayList<View> settingsModules = new ArrayList<>();
-
-        ArrayList<View> settingViews1 = new ArrayList<>();
-        settingViews1.add(new Switch(this));
-        settingViews1.add(new Switch(this));
-        settingViews1.add(new Switch(this));
-        settingViews1.add(new Switch(this));
-        settingViews1.add(new Switch(this));
-        ArrayList<View> settingViews2 = new ArrayList<>();
-        settingViews2.add(new Switch(this));
-        ArrayList<View> settingViews3 = new ArrayList<>();
-        settingViews3.add(new Switch(this));
-        settingViews3.add(new Switch(this));
-        CheckBox checkBox = new CheckBox(this);
-        checkBox.setPadding(MyTools.dpToPx(this,40),0,0,0);
-        settingViews3.add(checkBox);
-
-        settingsGroup = MyTools.customListFormat(settingViews1,this,true, MyTools.dpToPx(this, 2),true);
-        settingsModules.add(settingsGroup);
-        settingsGroup = MyTools.customListFormat(settingViews2,this,true, MyTools.dpToPx(this, 2),true);
-        settingsModules.add(settingsGroup);
-        settingsGroup = MyTools.customListFormat(settingViews3,this,true,MyTools.dpToPx(this, 2),true);
-        settingsModules.add(settingsGroup);
-
-        settingsGroup = MyTools.customListFormat(settingsModules,this,false,MyTools.dpToPx(this, 10),false);
-
-        settingsLinLay.addView(settingsGroup);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        navController = Navigation.findNavController(this, R.id.nav_controller_view_tag);
 
 
+        bottomNavigationView = findViewById(R.id.menuBottom);
+//        NavigationView navigationView = findViewById(R.id.na);
+//
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new Fragment()).commit();
+//            navigationView.setCheckedItem(R.id.item1);
+//        }
 
+        replaceFragment(new FirstFragment());
 
-
-
-        usernameButton = findViewById(R.id.usernameButton);
-        connectButton = findViewById(R.id.connectRoomButton);
-
-        usernameButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                SetUserNameDialog setUserNameDialog = new SetUserNameDialog();
-                setUserNameDialog.show(getSupportFragmentManager(),"Rename");
-            }
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.item1)
+                replaceFragment(new FirstFragment());
+            else if (id == R.id.item2)
+                replaceFragment(new SecondFragment());
+            else if (id == R.id.item3)
+                replaceFragment(new ThirdFragment());
+            return true;
         });
-
-
-        connectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Http http = new Http(context);
-                http.execute();
-            }
-        });
-
     }
 
-    @Override
-    public void getStringFromDialog(String value) {
-        usernameButton.setText(value);
+    private  void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
+
+
+
+//        setContentView(R.layout.profile);
+//
+//        context = this;
+//
+//        settingsLinLay = findViewById(R.id.settings_lin_lay);
+//
+//        ArrayList<View> settingsModules = new ArrayList<>();
+//
+//        ArrayList<View> settingViews1 = new ArrayList<>();
+//        settingViews1.add(new Switch(this));
+//        settingViews1.add(new Switch(this));
+//        settingViews1.add(new Switch(this));
+//        settingViews1.add(new Switch(this));
+//        settingViews1.add(new Switch(this));
+//        ArrayList<View> settingViews2 = new ArrayList<>();
+//        settingViews2.add(new Switch(this));
+//        ArrayList<View> settingViews3 = new ArrayList<>();
+//        settingViews3.add(new Switch(this));
+//        settingViews3.add(new Switch(this));
+//        CheckBox checkBox = new CheckBox(this);
+//        checkBox.setPadding(MyTools.dpToPx(this,40),0,0,0);
+//        settingViews3.add(checkBox);
+//
+//        settingsGroup = MyTools.customListFormat(settingViews1,this,true, MyTools.dpToPx(this, 2),true);
+//        settingsModules.add(settingsGroup);
+//        settingsGroup = MyTools.customListFormat(settingViews2,this,true, MyTools.dpToPx(this, 2),true);
+//        settingsModules.add(settingsGroup);
+//        settingsGroup = MyTools.customListFormat(settingViews3,this,true,MyTools.dpToPx(this, 2),true);
+//        settingsModules.add(settingsGroup);
+//
+//        settingsGroup = MyTools.customListFormat(settingsModules,this,false,MyTools.dpToPx(this, 10),false);
+//
+//        settingsLinLay.addView(settingsGroup);
+//
+//
+//
+//
+//
+//
+//        usernameButton = findViewById(R.id.usernameButton);
+//        connectButton = findViewById(R.id.connectRoomButton);
+//
+//        usernameButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                SetUserNameDialog setUserNameDialog = new SetUserNameDialog();
+//                setUserNameDialog.show(getSupportFragmentManager(),"Rename");
+//            }
+//        });
+//
+//
+//        connectButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Http http = new Http(context);
+//                http.execute();
+//            }
+//        });
+
+//        editText.addTextChangedListener(new TextWatcher() {
+//
+//            @Override
+//            public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) { }
+//            @Override
+//            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) { }
+//            @Override
+//            public void afterTextChanged(Editable et) {
+//                String s=et.toString();
+//                if(!s.equals(s.toUpperCase()))
+//                {
+//                    s=s.toUpperCase();
+//                    editText.setText(s);
+//                    editText.setSelection(editText.length()); //fix reverse texting
+//                }
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void getStringFromDialog(String value) {
+//        usernameButton.setText(value);
+//    }
+
+
 }
