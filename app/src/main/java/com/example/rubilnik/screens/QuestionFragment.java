b7fed59deb46d23cb5a1a67248a6d2f46c2cba8a
Art Monakhov <artemius135@gmail.com>
2023-12-15 19:03:28 +0300

    QuestionFragment: buttonsLayout,choiceButton

--- app/src/main/java/com/example/rubilnik/screens/QuestionFragment.java
+++ app/src/main/java/com/example/rubilnik/screens/QuestionFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,6 +18,8 @@ public class QuestionFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.question_fragment, container, false);
+        LinearLayout buttonsLayout = rootView.findViewById(R.id.buttonsLayout);
+        Button choiceButton;
 
         return rootView;
     }
