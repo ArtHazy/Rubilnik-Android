d60c915acba156764583197da8cc1d0fdc845ab5
Art Monakhov <artemius135@gmail.com>
2023-12-15 13:05:12 +0300

    JSONObject data

--- app/src/main/java/com/example/rubilnik/MainActivity.java
+++ app/src/main/java/com/example/rubilnik/MainActivity.java
@@ -39,6 +39,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -48,6 +49,7 @@ import io.socket.emitter.Emitter;
 
 import java.io.Console;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
@@ -79,11 +81,11 @@ public class MainActivity extends AppCompatActivity {
         }
         // Register event handlers
         mSocket.on(Socket.EVENT_CONNECT, onConnect);
-        mSocket.on("myEvent", onMyEvent);
         mSocket.on("joined", onJoined);
         mSocket.on("join", onJoin);
         mSocket.on("start", onStart);
         mSocket.on("next", onNext);
+        mSocket.on("choice", onChoice);
         mSocket.on("reveal", onReveal);
         mSocket.on("end", onEnd);
         mSocket.on("bark", onBark);
@@ -136,52 +138,91 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private Emitter.Listener onMyEvent = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            // Handle "myEvent" event
-        }
-    };
-
     private Emitter.Listener onJoined = new Emitter.Listener() {
         @Override
         public void call(Object... args) {
-            // Handle "myEvent" event
+            JSONObject data = (JSONObject) args[0];
+            String playerId;
+            try {
+                playerId = data.getString("playerId");
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
         }
     };
 
     private Emitter.Listener onJoin = new Emitter.Listener() {
         @Override
         public void call(Object... args) {
-            // Handle "myEvent" event
+            JSONObject data = (JSONObject) args[0];
+            String userName;
+            String userId;
+            try {
+                userName = data.getString("userName");
+                userId = data.getString("userId");
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
         }
     };
 
     private Emitter.Listener onStart = new Emitter.Listener() {
         @Override
         public void call(Object... args) {
-            // Handle "myEvent" event
+
         }
     };
 
     private Emitter.Listener onNext = new Emitter.Listener() {
         @Override
         public void call(Object... args) {
-            // Handle "myEvent" event
+            JSONObject data = (JSONObject) args[0];
+            try {
+                String text = data.getString("text");
+                JSONArray choices = data.getJSONArray("choices");
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
+
+    private Emitter.Listener onChoice = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            JSONObject data = (JSONObject) args[0];
+            try {
+                String userId = data.getString("userId");
+                int questionInd = data.getInt("questionInd");
+                int choiceInd = data.getInt("choiceInd");
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
         }
     };
 
     private Emitter.Listener onReveal = new Emitter.Listener() {
         @Override
         public void call(Object... args) {
-            // Handle "myEvent" event
+            JSONObject data = (JSONObject) args[0];
+            int choiceInd;
+            try {
+                choiceInd = data.getInt("choiceInd");
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
         }
     };
 
     private Emitter.Listener onEnd = new Emitter.Listener() {
         @Override
         public void call(Object... args) {
-            // Handle "myEvent" event
+            JSONObject data = (JSONObject) args[0];
+            JSONArray scores;
+            try {
+                scores = data.getJSONArray("scores");
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
         }
     };
 
@@ -192,7 +233,6 @@ public class MainActivity extends AppCompatActivity {
     private Emitter.Listener onBark = new Emitter.Listener() {
         @Override
         public void call(Object... args) {
-
             JSONObject data = (JSONObject) args[0];
             String msg;
             try {
